41f90afd7917de445d54e099d5deae6b69c9b694
Gabriel Farrell <g@grrawr.com>
2011-12-04 12:40:27 -0500

    Start on some real simple auth

--- server.js
+++ server.js
@@ -19,6 +19,42 @@ if (redisUrl) {
 
 var templates = snout.sniff(__dirname+'/templates');
 
+// from http://stackoverflow.com/questions/3498005/user-authentication-libraries-for-node-js
+var authCheck = function (req, res, next) {
+  var parsed = url.parse(req.url, true);
+
+  // Logout
+  if (parsed.pathname == "/logout") {
+    req.session.destroy();
+  }
+
+  // Is User already validated?
+  if (req.session && req.session.auth == true) {
+    next(); // stop here and pass to the next onion ring of connect
+    return;
+  }
+
+  // Auth - Replace this simple if with you Database or File or Whatever...
+  // If Database, you need a Async callback...
+  if (parsed.pathname == "/login" && 
+      parsed.query.name == "max" && 
+      parsed.query.pwd == "herewego") {
+    req.session.auth = true;
+    next();
+    return;
+  }
+
+  // User is not authorized. Stop talking to him.
+  if (parsed.pathname == "/admin") {
+    res.writeHead(403);
+    res.end('Sorry you are unauthorized.\n\nFor a login use: /login?name=max&pwd=herewego');
+    return;
+  }
+
+  // Pass on
+  next();
+};
+
 connect(
   connect.vhost('127.0.0.1|localhost|philly.sheltr.org', 
     connect(
@@ -29,6 +65,7 @@ connect(
         secret: process.env.SECRET || 'walrus'
       }),
       connect.query(),
+      authCheck,
       connect.router(function(app) {
         route(app);
       }),
@@ -65,6 +102,10 @@ function route(app) {
     res.writeHead(200, {'Content-Type': 'text/html'});
     res.end(render(templates.about));
   });
+  app.get('/admin', function(req, res, next) {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.end(render(templates.admin));
+  });
   app.get('/l/:id', function(req, res, next) {
     var id = req.params.id;
     https.get({
@@ -97,8 +138,6 @@ function route(app) {
       path: '/v1/app/60ecdcdd9fd6433297924f75c1c07b13/text?f=shelters_near&result_only=true&params={"center":['+req.query.lat+','+req.query.long+']}',
       headers: {'X-CloudMine-ApiKey': process.env.CMAPI}
     }, function(cmres) {
-      console.log(cmres.statusCode);
-      console.log(cmres.headers);
       res.writeHead(200, {'Content-Type': 'application/json'});
       cmres.on('data', function(chunk) {
         res.write(chunk);
