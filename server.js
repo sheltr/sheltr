7fa6f6bb19bd84785eea1721ee81275fdd2cf5a9
Gabriel Farrell <g@grrawr.com>
2011-12-04 11:07:36 -0500

    Handle heroku redis url

--- server.js
+++ server.js
@@ -2,9 +2,22 @@ var connect = require('connect');
 var https = require('https');
 var snout = require('snout');
 var whiskers = require('whiskers');
+var url = require('url');
 var RedisStore = require('connect-redis')(connect);
 
 var port = process.env.PORT || 8127;
+var redisUrl = process.env.REDISTOGO_URL && url.parse(process.env.REDISTOGO_URL);
+var redisStore;
+
+if (redisUrl) {
+  redisStore = new RedisStore({
+    host: redisUrl.host,
+    port: redisUrl.port,
+    pass: redisURL.auth.split(':')[1]
+  });
+} else {
+  redisStore = new RedisStore;
+}
 
 var templates = snout.sniff(__dirname+'/templates');
 
@@ -13,7 +26,7 @@ connect(
     connect(
       connect.cookieParser(),
       connect.session({
-        store: new RedisStore, 
+        store: redisStore, 
         secret: process.env.SECRET || 'walrus'
       }),
       connect.logger(),
@@ -73,6 +86,23 @@ function route(app) {
         res.writeHead(200, {'Content-Type': 'text/html'});
         loc.raw = JSON.stringify(loc);
         res.end(render(templates.location, {loc: loc}));
+      });
+    }).on('error', function(e) {
+      console.log(e);
+      next();
+    });
+  });
+  app.get('/_map', function(req, res, next) {
+    https.get({
+      host: 'api.cloudmine.me',
+      path: '/v1/app/60ecdcdd9fd6433297924f75c1c07b13/text?f=shelters_near&apikey=1d0c51df5b5947059c018e9305e3fa69&result_only=true&params={"center": ['+req.query.lat+', '+req.query.long+']',
+      headers: {'X-CloudMine-ApiKey': process.env.CMAPI}
+    }, function(cmres) {
+      res.writeHead(200, {'Content-Type': 'application/json'});
+      cmres.on('data', function(chunk) {
+        res.write(chunk);
+      }).on('end', function() {
+        res.end();
       });
     }).on('error', function(e) {
       console.log(e);
