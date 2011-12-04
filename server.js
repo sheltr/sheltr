d6b1d428c55f2308aa3076f4718b6d349ae122dc
Gabriel Farrell <g@grrawr.com>
2011-12-04 11:27:24 -0500

    Keep on truckin2

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ var redisOptions;
 
 if (redisUrl) {
   redisOptions = {
-    host: redisUrl.host,
+    host: redisUrl.hostname,
     port: redisUrl.port,
     pass: redisUrl.auth.split(':')[1]
   };
