8a7efa38f749d0799b7e8a968556731754ff0590
Mike Ball <mikedball@gmail.com>
2011-12-03 19:03:10 -0500

    SH.map tweaked to orient user based on W3C geolocation and to also drop a draggable marker on the user's location.

--- static/js/sh.map.js
+++ static/js/sh.map.js
@@ -11,11 +11,11 @@ if (typeof SH === 'undefined' || !SH) {
     _self = {
       create: function (options) {
         var settings = {
-            mapId: 'map',
-            zoom: 12,
-				    center: new google.maps.LatLng(39.95240, -75.16362), 
-            mapTypeId: google.maps.MapTypeId.ROADMAP
-          };
+              mapId: 'map',
+              zoom: 14,
+              center: new google.maps.LatLng(39.95240, -75.16362),
+              mapTypeId: google.maps.MapTypeId.ROADMAP
+            };
 
         if (options) {
           $.extend(settings, options);
@@ -35,35 +35,57 @@ if (typeof SH === 'undefined' || !SH) {
           lng = shelters.shelters[i].lng;
           latlng = new google.maps.LatLng(lat, lng);
 
-				  this.createMarker(latlng, shelters.shelters[i].desc);
+				  _self.createMarker(latlng, shelters.shelters[i].desc);
         }
       },
 
-      createMarker: function (latlng, description) {
-        marker = new google.maps.Marker({
-          position: latlng,
-          map: map
-        });
+      createMarker: function (latlng, description, options) {
+        var settings = {
+              position: latlng,
+              map: map,
+          };
 
+        if (options) {
+          $.extend(settings, options);
+        }
+
+        marker = new google.maps.Marker(settings);
         google.maps.event.addListener(marker, 'click', function() {
-          infowindow.close();
-          infowindow.setContent(description);
-          infowindow.open(map,marker);
+          infoWindow.close();
+          infoWindow.setContent(description);
+          infoWindow.open(map,marker);
         });
+        
+        if (description === 'You!') { //TODO: this is fragile.
+          google.maps.event.addListener(marker, 'dragend', function () {
+            console.log('dragend event listener worked!');
+          });
+        }
       },
 
-      locateUser: function () {
+      orientUser: function () {
         var userLocation = null;
 
         // Try W3C Geolocation
         if (navigator.geolocation) {
           navigator.geolocation.getCurrentPosition(function(position) {
-            userLocation = new google.maps.LatLng(position.coords.latitude,position.coords.longitude);
-            return userLocation;
+            userLocation = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+            _self.create({ center: userLocation });
+            _self.addShelters(shelters);
+            _self.createMarker(userLocation, 'You!', {
+              animation: google.maps.Animation.DROP,
+              draggable: true
+            });
           }, function() {
-            return userLocation;
+            _self.create();
           });
         }
+
+        return userLocation;
+      },
+
+      updateMapCenter: function (latLongObj) {
+        map.setCenter(latLongObj);
       }
     };
 
