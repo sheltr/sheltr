7ff0732c0c58ac0a2fc0383e2c18bbcb9c377991
Casey Thomas <cpthomas@gmail.com>
2012-04-24 23:55:39 -0400

    Fixed in page map links

--- static/js/sh.locations.js
+++ static/js/sh.locations.js
@@ -47,7 +47,7 @@ sheltr.locations = (function ($) {
 
       $('ul.locations > li > a').click(function(){
         sheltr.map.zoomToMarker($(this).attr('id'));
-        return false;
+        window.location.hash = '#map';
       });
     },
 
