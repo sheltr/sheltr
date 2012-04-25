9a0085e4186dee69d689d23ca08fe76ddc08f2e8
Gabriel Farrell <g@grrawr.com>
2012-04-25 00:05:23 -0400

    Show available beds in location list

--- static/js/sh.locations.js
+++ static/js/sh.locations.js
@@ -16,15 +16,20 @@ sheltr.locations = (function ($) {
   }
 
   function buildLocationsHTML(location) {
-    var name = location.name ? location.name : '',
+    var name = location.name || '',
         addr = location.address1,
         id =  location.id,
         icon = _self.selectMarkerIcon(location),
         url = location.id,
-        stat = location.status ? location.status : '',
-        distance = _self.distanceFromUser(location);
+        stat = location.status || '',
+        distance = _self.distanceFromUser(location),
+        beds = '';
+  
+    if (location.totalBeds) {
+      beds = '<p>Available beds: ' + location.openBeds + '</p>';
+    }
 
-    return '<li><img src="' + icon + '" /><h2><a href="' + url + '">' + name + '</a></h2><address>' + addr + '</address><p>Distance: ' + distance + ' miles</p>' + hasStatus(stat) + '<a href="#map" id="' + id + '">(View map)</span></li>';
+    return '<li><img src="' + icon + '" /><h2><a href="' + url + '">' + name + '</a></h2><address>' + addr + '</address>'+ beds + '<p>Distance: ' + distance + ' miles</p>' + hasStatus(stat) + '<a href="#map" id="' + id + '">(View map)</span></li>';
   }
 
   _self = {
