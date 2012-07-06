8d109f2215b2e02ea2e1e9bf1f41e32a06e44a3d
Gabriel Farrell <g@grrawr.com>
2012-07-06 08:46:45 -0400

    Changed fn to cb in db

--- db/redis.js
+++ db/redis.js
@@ -8,40 +8,40 @@ client.on('error', function (err) {
 // If no key specified, use filename for some degree of uniqueness
 var key = process.env.DBKEY || __filename;
 
-exports.get = function(id, fn) {
+exports.get = function(id, cb) {
   client.get(key, function(err, reply) {
-    if (err) return fn(err);
+    if (err) return cb(err);
     console.log(reply);
     reply = JSON.parse(reply);
-    if (!reply || !reply[id]) return fn(new Error('Not found'));
-    fn(null, reply[id]);
+    if (!reply || !reply[id]) return cb(new Error('Not found'));
+    cb(null, reply[id]);
   });
 };
 
-exports.post = function(docs, fn) {
+exports.post = function(docs, cb) {
   // uncomment to reset
   //client.del(key);
   client.get(key, function(err, reply) {
-    if (err) return fn(err);
+    if (err) return cb(err);
     reply = JSON.parse(reply) || {};
     merge(reply, docs);
     client.set(key, JSON.stringify(reply), function(err, setReply) {
-      if (err) return fn(err);
-      fn(null, setReply);
+      if (err) return cb(err);
+      cb(null, setReply);
     });
   });
 };
 
-exports.near = function(lat, lon, limit, fn) {
+exports.near = function(lat, lon, limit, cb) {
   var _limit;
-  if (fn) {
+  if (cb) {
     _limit = limit || 20;
   } else {
-    fn = limit;
+    cb = limit;
     _limit = 20;
   }
   client.get(key, function(err, reply) {
-    if (err) return fn(err);
+    if (err) return cb(err);
     var db = JSON.parse(reply);
     var doc, distance, distances = [];
     for (var id in db) {
@@ -56,38 +56,38 @@ exports.near = function(lat, lon, limit, fn) {
       //locObj[id].id = id;
       //locations.push(locObj[id]);
     }
-    fn(null, distances);
+    cb(null, distances);
   });
 };
 
-exports.loc = function(idOrSlug, fn) {
+exports.loc = function(idOrSlug, cb) {
   exports.get(idOrSlug, function(err, data) {
-    if (err) return exports.getBySlug(idOrSlug, fn);
-    fn(null, data);
+    if (err) return exports.getBySlug(idOrSlug, cb);
+    cb(null, data);
   });
 };
 
-exports.getBySlug = function(slug, fn) {
+exports.getBySlug = function(slug, cb) {
   //request({
   //  url: encodeURI('https://api.cloudmine.me/v1/app/'+process.env.CMID+'/search?q=[slug="'+slug+'"]'),
   //  headers: {'X-CloudMine-ApiKey': process.env.CMKEY}
   //}, function(err, res, body) {
-  //  if (err) return fn(err);
-  //  if (_.isEmpty(JSON.parse(body).success)) return fn(body);
-  //  fn(null, body);
+  //  if (err) return cb(err);
+  //  if (_.isEmpty(JSON.parse(body).success)) return cb(body);
+  //  cb(null, body);
   //});
 };
 
-exports.settings = function(doc, fn) {
+exports.settings = function(doc, cb) {
   // get settings if no doc param
-  if (!fn) {
-    fn = doc;
+  if (!cb) {
+    cb = doc;
     return exports.get('settings', function(err, data){
-      if (err) return fn(err);
-      fn(null, data);
+      if (err) return cb(err);
+      cb(null, data);
     });
   }
-  exports.post({settings: doc}, fn);
+  exports.post({settings: doc}, cb);
 };
 
 function getDistance(x1, y1, x2, y2) {
