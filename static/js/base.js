2f801e2c37b73c7c35ccb87433192334f08e4a76
Gabriel Farrell <g@grrawr.com>
2012-07-17 00:30:37 -0400

    Default settings should include region
    Not the old city, state business.

--- static/js/base.js
+++ static/js/base.js
@@ -11,7 +11,7 @@ $(function() {
         box.text(JSON.stringify(data, null, 2))
       }, 
       error: function() {
-        box.text('{\n  "boundingBox": [\n    [39.8480851, -75.395736],\n    [40.15211, -74.863586]\n  ],\n  "minZoom": 12,\n  "mapCenter": [39.95240, -75.16362],\n  "city": "Philadelphia",\n  "state": "PA"\n}')
+        box.text('{\n  "boundingBox": [\n    [39.8480851, -75.395736],\n    [40.15211, -74.863586]\n  ],\n  "minZoom": 12,\n  "mapCenter": [39.95240, -75.16362],\n  "region": "Philadelphia, PA"\n}')
       }
     })
     form.on('submit', function() {
